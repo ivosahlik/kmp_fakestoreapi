06ed293c897834d2e448ef66dc6f6ad2b9f8d94d
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2024-07-02 00:13:10 +0200

    added new colum, texts an row

--- androidApp/src/main/java/cz/ivosahlik/kmp_fakestoreapi/android/View/ProductCard.kt
+++ androidApp/src/main/java/cz/ivosahlik/kmp_fakestoreapi/android/View/ProductCard.kt
@@ -1,20 +1,29 @@
 package cz.ivosahlik.kmp_fakestoreapi.android.View
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import cz.ivosahlik.kmp_fakestoreapi.Product
+import androidx.compose.ui.layout.ContentScale.Companion as ContentScale1
 
 @Composable
 fun ProductCard(product: Product) {
@@ -32,8 +41,44 @@ fun ProductCard(product: Product) {
                     .crossfade(true)
                     .build(),
                 contentDescription = "Product Image",
-                contentScale = ContentScale.Crop
+                contentScale = ContentScale1.Crop
             )
+            Column(modifier = Modifier.padding(all = 10.dp)) {
+                Text(
+                    modifier = Modifier.padding(vertical = 10.dp),
+                    text = product.title,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    text = product.description,
+                    maxLines = 6,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 12.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    SuggestionChip(
+                        shape = CircleShape,
+                        onClick = { },
+                        label = {
+                            Text(text = product.category)
+                        }
+                    )
+                    Text(
+                        text = "$${product.price}",
+                        fontWeight = FontWeight.Medium,
+                        fontSize = MaterialTheme.typography.bodyMedium.fontSize
+                    )
+                }
+            }
         }
     }
 }
